0223d16fded115cf70597fe25588eaa137d83a1a
Jawad <jawad.olavian@gmail.com>
2026-06-11 21:39:27 +0100

    Initial setup
    Happy Birthday, Mr Savage!

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # HAMJ-Racecar
 The repository for the MicroPython code on our Electronics 1 racecar. Team HAMJ 2026 
+
+The more the turn input button is held down, the more the servo should adjust to, up to a limit
--- race.py
+++ race.py
@@ -2,4 +2,21 @@
 from machine import Pin, PWM
 import time
 
-#code etc
+#PINS
+servo_pin = 23
+motor_pin1 = 16
+motor_pin2 = 17
+
+#PWM setup
+servo = PWM(Pin(servo_pin), freq=50)
+fmotor = PWM(Pin(motor_pin1), freq=10000)
+bmotor = PWM(Pin(motor_pin2), freq=10000)
+
+def turn_default():
+    servo.duty(70)
+
+def turn_right():
+    servo.duty(120)
+
+def turn_left():
+    servo.duty(20)
